997f2d94737c9a8a02827782b1d9b7f22a6189b3
AnishPahilajani <anishhp13@gmail.com>
2023-11-25 20:04:01 -0800

    chanigng model for semeval

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class InputEmbeddings(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size, d_model)# A simple lookup table that stores embeddings of a fixed dictionary and size.
+        self.embedding = nn.Embedding(vocab_size, 1)# A simple lookup table that stores embeddings of a fixed dictionary and size.
         
     def forward(self, x):
         return self.embedding(x)/math.sqrt(self.d_model)
